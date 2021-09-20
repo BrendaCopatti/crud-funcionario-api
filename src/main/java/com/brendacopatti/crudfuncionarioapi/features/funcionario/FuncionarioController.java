a188a455eb028cc7b91b7662f08cd8bc735ba25d
Brenda <brendascopatti@hotmail.com>
2021-09-19 21:12:44 -0300

    CRUD funcional

--- src/main/java/com/brendacopatti/crudfuncionarioapi/features/funcionario/FuncionarioController.java
+++ src/main/java/com/brendacopatti/crudfuncionarioapi/features/funcionario/FuncionarioController.java
@@ -3,14 +3,32 @@ package com.brendacopatti.crudfuncionarioapi.features.funcionario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/api/funcionario")
 public class FuncionarioController {
 
     @Autowired
     private FuncionarioRepository repository;
 
-    @GetMapping(path = "/api/funcionario/{id}")
+    @PostMapping
+    public Funcionario post(@RequestBody Funcionario funcionario) {
+        return repository.save(funcionario);
+    }
+
+    @GetMapping
+    public List<Funcionario> getAll() {
+        List<Funcionario> funcionarios = new ArrayList<>();
+        repository.findAll().forEach(funcionarios::add);;
+        return funcionarios;
+    }
+
+    @GetMapping(path = "/{id}")
     public ResponseEntity get(@PathVariable("id") Long id) {
         return repository.findById(id).map(r ->
                 ResponseEntity.ok().body(r)).orElse(
@@ -18,9 +36,17 @@ public class FuncionarioController {
         );
     }
 
-    @PostMapping(path = "/api/funcionario")
-    public Funcionario post(@RequestBody Funcionario funcionario) {
+    @PutMapping(path = "/{id}")
+    public Funcionario update(@PathVariable("id") Long id, @RequestBody Funcionario funcionario) {
+        if (!repository.existsById(id)) {
+            throw new RestClientException("Não foi encontrado registro com o identificador informado.");
+        }
         return repository.save(funcionario);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        repository.deleteById(id);
     }
 
 }
